1cb43d3098b88a240035e7dd325e9b0a29f83a53
aldeez988 <aldeez911088@gmail.com>
2019-07-11 23:39:16 +0100

    removing key to env file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
+import dotenv from "dotenv";
 
-const KEY = 'AIzaSyDz77n67RnXvIEtcWGKdVEYM9Xt4LObRzY';
+dotenv.config();
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: process.env.KEY
+  }
 });
-
-
