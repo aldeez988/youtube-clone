2b057f65737fd2d9a7642612e94d5de6c2b95875
aldeez988 <aldeez911088@gmail.com>
2019-07-14 03:45:26 +0100

    fixing key issue

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: process.env.KEY
+    key: process.env.REACT_APP_KEY
   }
 });
